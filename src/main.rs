c552768614f5877c7c79e6146fa113b66b9a95bf
andrey.khalyavin <andrey.khalyavin@ural.rt.ru>
2024-11-09 14:03:39 +0500

    took ready parser with lexer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 mod parser;
 
 fn main() {
+    print!("{}", 1);
+    let lexer = parser::lexer::Lexer::new("SELECT col1 FROM table1");
+
+    let ast = parser::Parser::new(lexer).parse();
+
+    // let logical_plan = analyzer::Analyzer::new().analyze(&ast);
+
+    print!("{:?}", ast);
 }
